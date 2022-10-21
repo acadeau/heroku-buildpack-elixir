868868d4758b359b9792a185d4b9783dbe874d82
Alexandre Cadeau <alexandre.cadeau@protonmail.com>
2022-10-21 19:14:33 +0200

    Update canonical_version.sh

--- lib/canonical_version.sh
+++ lib/canonical_version.sh
@@ -8,6 +8,9 @@ erlang_builds_url() {
     "heroku-22")
       erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-22.04"
       ;;
+    "scalingo-20")
+      erlang_builds_url="https://repo.hex.pm/builds/otp/ubuntu-20.04"
+      ;;
     *)
       erlang_builds_url="https://s3.amazonaws.com/heroku-buildpack-elixir/erlang/cedar-14"
       ;;
@@ -28,6 +31,10 @@ fetch_erlang_versions() {
       ;;
     "heroku-22")
       url="https://repo.hex.pm/builds/otp/ubuntu-22.04/builds.txt"
+      curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
+      ;;
+    "scalingo-20")
+      url="https://repo.hex.pm/builds/otp/ubuntu-20.04/builds.txt"
       curl -s "$url" | awk '/^OTP-([0-9.]+ )/ {print substr($1,5)}'
       ;;
     *)
